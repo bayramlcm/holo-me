7c6e16be4954fad789f2a4a5a9d0f853880b0c15
Bayram Alaçam <bayram.alacam@walue.ws>
2020-05-20 16:07:16 +0300

    Create personal

--- cc_tuts/zomes/hello/code/src/lib.rs
+++ cc_tuts/zomes/hello/code/src/lib.rs
@@ -1,33 +1,61 @@
 #![feature(proc_macro_hygiene)]
+#[macro_use]
 extern crate hdk;
 extern crate hdk_proc_macros;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
+#[macro_use]
 extern crate holochain_json_derive;
+use hdk::{entry_definition::ValidatingEntryType, error::ZomeApiResult};
 
-use hdk::{
-    error::ZomeApiResult,
-};
+use hdk::holochain_core_types::{dna::entry_types::Sharing, entry::Entry};
+
+use hdk::holochain_json_api::{error::JsonError, json::JsonString};
+
+use hdk::holochain_persistence_api::cas::content::Address;
 
 use hdk_proc_macros::zome;
 
+#[derive(Serialize, Deserialize, Debug, DefaultJson, Clone)]
+pub struct Person {
+    name: String,
+}
+
 #[zome]
 mod hello_zome {
-
     #[init]
     fn init() {
         Ok(())
     }
-
     #[validate_agent]
     pub fn validate_agent(validation_data: EntryValidationData<AgentId>) {
         Ok(())
     }
 
     #[zome_fn("hc_public")]
-    pub fn hello_holo() -> ZomeApiResult<String> {
-        Ok("Merhaba Holo".into())
+    fn hello_holo() -> ZomeApiResult<String> {
+        Ok("Hello Holo".into())
+    }
+    #[entry_def]
+    fn person_entry_def() -> ValidatingEntryType {
+        entry!(
+            name: "person",
+            description: "Person to say hello to",
+            sharing: Sharing::Private,
+            validation_package: || {
+                hdk::ValidationPackageDefinition::Entry
+            },
+            validation: | _validation_data: hdk::EntryValidationData<Person>| {
+                Ok(())
+            }
+        )
+    }
+    #[zome_fn("hc_public")]
+    pub fn create_person(person: Person) -> ZomeApiResult<Address> {
+        let entry = Entry::App("person".into(), person.into());
+        let address = hdk::commit_entry(&entry)?;
+        Ok(address)
     }
 }
