ce20599bb18bfc6b5c1e870cfd3952a7d0b1837f
Bayram Alaçam <bayram.alacam@walue.ws>
2020-05-20 16:19:59 +0300

    Kullanıcı oluşturma servisi bağlandı

--- gui/hello.js
+++ gui/hello.js
@@ -13,3 +13,11 @@ function show_output(result) {
   var output = JSON.parse(result);
   span.textContent = ' ' + output.Ok;
 }
+function create_person() {
+  const name = document.getElementById('name').value;
+  holochain_connection.then(({callZome, close}) => {
+    callZome('test-instance', 'hello', 'create_person')({
+      person: {name: name},
+    }).then(result => console.log(result));
+  });
+}
