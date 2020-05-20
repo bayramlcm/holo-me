ac8b33c4ebe8fdfa093a7c25c46852cf99f92875
Bayram Alaçam <bayram.alacam@walue.ws>
2020-05-20 16:22:50 +0300

    Kullanıcı oluşturdan sonra dönen adresin siteye yansıtılması

--- gui/hello.js
+++ gui/hello.js
@@ -1,23 +1,27 @@
 var holochain_connection = holochainclient.connect();
 function hello() {
-  holochain_connection.then(({callZome, close}) => {
+  holochain_connection.then(({ callZome, close }) => {
     callZome(
       'test-instance',
       'hello',
       'hello_holo',
-    )({args: {}}).then(result => show_output(result));
+    )({ args: {} }).then(result => show_output(result, 'output'));
   });
 }
-function show_output(result) {
-  var span = document.getElementById('output');
+function show_output(result, id) {
+  var el = document.getElementById(id);
   var output = JSON.parse(result);
-  span.textContent = ' ' + output.Ok;
+  if (output.Ok) {
+    el.textContent = ' ' + output.Ok;
+  } else {
+    alert(output.Err.Internal);
+  }
 }
 function create_person() {
   const name = document.getElementById('name').value;
-  holochain_connection.then(({callZome, close}) => {
+  holochain_connection.then(({ callZome, close }) => {
     callZome('test-instance', 'hello', 'create_person')({
-      person: {name: name},
-    }).then(result => console.log(result));
+      person: { name: name },
+    }).then(result => show_output(result, 'address_output'));
   });
 }
