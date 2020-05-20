3c58ef02e606dd3de01261e7b9da324af3ec29eb
Bayram Alaçam <bayram.alacam@walue.ws>
2020-05-20 15:03:52 +0300

    Test yazılması

--- README.md
+++ README.md
@@ -3,21 +3,36 @@
 ### Holochain ile yapılmış server - client (web) ilişkili uygulama
 
 ## Gerekli Hazırlıklar
+
 * [Nix](https://nixos.org/) kurulumu yapılmalıdır.
 
 ## Nasıl çalıştırılır
+
 ### Nix ortamını hazırlama
-```console
+
+``` console
 $ nix-shell https://holochain.love
 ```
+
 ### Paketleri derleme
-```console
+
+``` console
 $ cd cc_tuts
 $ hc package
 ```
+
+### Test
+
+``` console
+$ hc test
+```
+
 ### Uygulamayı başlatma
-```console
+
+``` console
 $ hc run
 ```
+
 ### Uygulamayı önizleme
+
 #### [http://127.0.0.1:8888/](http://127.0.0.1:8888/)
--- cc_tuts/test/index.js
+++ cc_tuts/test/index.js
@@ -48,6 +48,23 @@ orchestrator.registerScenario('Test hello holo', async (s, t) => {
     const result = await alice.call('cc_tuts', 'hello', 'hello_holo', {});
     t.ok(result.Ok);
     t.deepEqual(result, { Ok: 'Merhaba Holo' });
+
+    const create_result = await alice.call('cc_tuts', 'hello', 'create_person', {
+        person: { name: 'Alice' },
+    });
+    t.ok(create_result.Ok);
+    const alice_person_address = create_result.Ok;
+
+    await s.consistency();
+
+    const retrieve_result = await alice.call(
+        'cc_tuts',
+        'hello',
+        'retrieve_person',
+        { address: alice_person_address },
+    );
+    t.ok(retrieve_result.Ok);
+    t.deepEqual(retrieve_result, { Ok: { name: 'Bayram' } });
 });
 
 
