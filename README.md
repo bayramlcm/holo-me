2e39739581f91aae3ad1d34ef028fa0117a12185
Bayram Alaçam <bayram.alacam@walue.ws>
2020-05-20 15:01:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Holochain Coreconcept
+
+### Holochain ile yapılmış server - client (web) ilişkili uygulama
+
+## Gerekli Hazırlıklar
+* [Nix](https://nixos.org/) kurulumu yapılmalıdır.
+
+## Nasıl çalıştırılır
+### Nix ortamını hazırlama
+```console
+$ nix-shell https://holochain.love
+```
+### Paketleri derleme
+```console
+$ cd cc_tuts
+$ hc package
+```
+### Uygulamayı başlatma
+```console
+$ hc run
+```
+### Uygulamayı önizleme
+#### [http://127.0.0.1:8888/](http://127.0.0.1:8888/)
+
+
+## Ekran Görüntüleri
+### Adım 1
+<img src="./screenshot/start.png">
+
+### Adım 2
+<img src="./screenshot/last.png">
---
+++ cc_tuts/test/index.js
@@ -0,0 +1,54 @@
+const path = require('path');
+
+const {
+    Orchestrator,
+    Config,
+    combine,
+    singleConductor,
+    localOnly,
+    tapeExecutor,
+} = require('@holochain/tryorama');
+
+process.on('unhandledRejection', error => {
+    // Will print "unhandledRejection err is not defined"
+    console.error('got unhandledRejection:', error);
+});
+
+const dnaPath = path.join(__dirname, '../dist/cc_tuts.dna.json');
+
+const orchestrator = new Orchestrator({
+    middleware: combine(
+        // use the tape harness to run the tests, injects the tape API into each scenario
+        // as the second argument
+        tapeExecutor(require('tape')),
+
+        // specify that all "players" in the test are on the local machine, rather than
+        // on remote machines
+        localOnly,
+    ),
+});
+
+const dna = Config.dna(dnaPath, 'cc_tuts');
+
+const config = Config.gen(
+    {
+        cc_tuts: dna,
+    },
+    {
+        network: {
+            type: 'sim2h',
+            sim2h_url: 'ws://localhost:9000',
+        },
+    },
+);
+
+orchestrator.registerScenario('Test hello holo', async (s, t) => {
+    const { alice, bob } = await s.players({ alice: config, bob: config }, true);
+
+    const result = await alice.call('cc_tuts', 'hello', 'hello_holo', {});
+    t.ok(result.Ok);
+    t.deepEqual(result, { Ok: 'Merhaba Holo' });
+});
+
+
+orchestrator.run();
